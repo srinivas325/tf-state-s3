f6d308883a7efefe0c7ab308c8f74753210d2c24
Naga Sunkara <59291856+srinivas325@users.noreply.github.com>
2024-06-05 00:42:28 +0100

    Delete variables.tf

--- variables.tf
+++
@@ -1,9 +0,0 @@
-variable "bucket_name" {
-  description = "The name of the S3 bucket"
-  type        = string
-}
-
-resource "aws_s3_bucket" "example" {
-  bucket = var.bucket_name
-  acl    = "private"
-}
