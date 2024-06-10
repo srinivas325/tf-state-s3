6f325c5b9d7ddae12fc2ce94d989c8b5fe0d8f37
Naga Sunkara <sns.srinivasa@gmail.com>
2024-06-10 05:08:20 +0100

    test

---
+++ dev.tfvars
@@ -0,0 +1,2 @@
+region      = "us-west-1"
+bucket_name = "dev-example-bucket"
--- main.tf
+++ main.tf
@@ -1,47 +1,16 @@
 provider "aws" {
-  region = var.aws_region
+  region = var.region
 }
 
-resource "aws_s3_bucket" "terraform_state_bucket" {
+resource "aws_s3_bucket" "bucket" {
   bucket = var.bucket_name
-
-  tags = {
-    Name        = var.bucket_name
-    Environment = var.environment
-  }
+  acl    = "private"
 }
 
-resource "aws_dynamodb_table" "terraform_state_lock_table" {
-  name           = var.lock_table_name
-  billing_mode   = "PAY_PER_REQUEST"
-  hash_key       = "LockID"
-
-  attribute {
-    name = "LockID"
-    type = "S"
-  }
-}
-
-variable "aws_region" {
-  description = "The AWS region where resources will be created"
-  type        = string
-  default     = "us-west-2"  // You can change the default value as needed
+variable "region" {
+  description = "AWS region"
 }
 
 variable "bucket_name" {
-  description = "The name of the S3 bucket to be created for Terraform state"
-  type        = string
-  default = "test"
-}
-
-variable "environment" {
-  description = "The environment for which the S3 bucket is created (e.g., development, staging, production)"
-  type        = string
-  default     = "development"  // You can change the default value as needed
-}
-
-variable "lock_table_name" {
-  description = "The name of the DynamoDB table for Terraform state locking"
-  type        = string
-  default = "terraform-dynamo-db-table"
+  description = "Name of the S3 bucket"
 }
